23da015f035b4481149341ce1a6ae06de5f72203
Dhruv Kumar <your-email@gmail.com>
2026-06-22 17:05:28 +0530

    Updated Flipkart homepage

--- src/components/productsection/productsection.jsx
+++ src/components/productsection/productsection.jsx
@@ -67,7 +67,7 @@ function ProductSection() {
 
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
 
         {products.map((product) => (
           <ProductCard
